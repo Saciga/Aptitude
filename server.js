9142e4b19ec74de06b19e9b2919eb5863d4dc499
Saciga <162351281+Saciga@users.noreply.github.com>
2025-04-26 18:22:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 
 // Smart CORS Configuration: allow only your frontend URL
 const corsOptions = {
-  origin: process.env.CLIENT_URL || 'http://localhost:3000', // Allow localhost during local development, frontend during production
+  origin: process.env.CLIENT_URL || 'https://680cb3d6459282de046905a5--endearing-dieffenbachia-83f5c4.netlify.app/', // Allow localhost during local development, frontend during production
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
